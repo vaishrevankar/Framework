51897381e1e1fa9547fb0b60fb31a838f41202f4
vaishrevankar <55649564+vaishrevankar@users.noreply.github.com>
2019-12-02 14:44:36 +0530

    Update BrowserFactory.java

--- Framework/src/test/java/com/learnautomation/utility/BrowserFactory.java
+++ Framework/src/test/java/com/learnautomation/utility/BrowserFactory.java
@@ -14,7 +14,7 @@ public class BrowserFactory {
 	{
 		if(browserName.equals("Chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver.exe");
 			driver=new ChromeDriver();
 			
 		}
